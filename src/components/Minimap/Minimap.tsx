6eb5597dfdb7a9a34531574c8a207e1926f40c4f
aebel-shajan <aebelshajan@gmail.com>
2025-02-05 20:44:13 +0000

    Add show hide button to minimap.

--- src/components/Minimap/Minimap.tsx
+++ src/components/Minimap/Minimap.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import styles from "./Minimap.module.css"
 
 const Minimap = () => {
+  const [show, setShow] = useState(true)
 
+  const showMinimap = () => setShow(true)
+  const hideMinimap = () => setShow(false)
+
+  if (!show) {
+    return (
+      <div className={styles.hiddenContainer}>
+        
+          <button onClick={showMinimap}>show</button>
+        
+      </div>
+    )
+  }
 
   return (
     <div className={styles.container}>
+      <div className={styles.options}>
+        <button onClick={hideMinimap}>close</button>
+        <button>option</button>
+      </div>
     </div>
   );
 }
- 
+
 export default Minimap;
