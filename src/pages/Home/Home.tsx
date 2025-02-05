fdab310420c2d87d5e5b3982194d29a38d1e6506
aebel-shajan <aebelshajan@gmail.com>
2025-02-05 20:11:56 +0000

    Setup boilerplate for testing minimap component.

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,25 @@
+
+const RandomColor = ({height=100}) => {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return (
+    <div style={{
+        backgroundColor: randomColor,
+        width: '100%',
+        height: `${height}vh`
+        }}
+      >
+    </div>
+  )
+}
+
+const Home = () => {
+  const randomColors = 
+  Array.from({ length: 5 }, (_, index) => <RandomColor key={index} />)
+  return (
+    <div>
+      {randomColors}
+    </div>
+  );
+}
+ 
+export default Home;
