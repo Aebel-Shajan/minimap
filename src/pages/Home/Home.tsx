f205335833a4e1e1ef60aeb9fe90d8fb319fd036
aebel-shajan <aebelshajan@gmail.com>
2025-02-07 17:50:34 +0000

    Add css to scroll container to have overflow-y: scroll

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Minimap from "../../components/Minimap/Minimap";
 import { useEffect, useState } from "react";
 import TestComponent from "./TestComponent/TestComponent";
+import styles from "./Home.module.css"
 
 const Home = () => {
   const [elementToMap, setElementToMap] = useState<HTMLElement | null>(null);
@@ -12,7 +13,10 @@ const Home = () => {
   const randomColors = 
   Array.from({ length: 50 }, (_, index) => <TestComponent key={index} />)
   return (
-    <div id="scroll-container">
+    <div 
+      id="scroll-container"
+      className={styles.scrollContainer}
+    >
       {randomColors}
       {elementToMap && <Minimap elementToMap={elementToMap}/>}
     </div>
