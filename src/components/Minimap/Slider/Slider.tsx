66bff647fefb0ee06974a8db45a689a0bdc18a7e
aebel-shajan <aebelshajan@gmail.com>
2025-02-11 17:34:56 +0000

    Add jsdocs for slider component.

--- src/components/Minimap/Slider/Slider.tsx
+++ src/components/Minimap/Slider/Slider.tsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from "react";
 import styles from "./Slider.module.css"
 
+/**
+ * Slider component that handles dragging functionality.
+ *
+ * @param {Object} props - The properties object.
+ * @param {number} props.sliderHeight - The height of the slider.
+ * @param {number} props.scrollTop - The top scroll position of the slider relative to
+ *    parent container.
+ * @param {CallableFunction} props.handleDrag - The function to handle drag events.
+ *    
+ * @returns {JSX.Element} The rendered Slider component.
+ *
+ * @component
+ */
 const Slider = (
   {
     sliderHeight,
