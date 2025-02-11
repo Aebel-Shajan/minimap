c907484ef0cf8c5942dfe109f2ea0c4dc4ed0dd5
aebel-shajan <aebelshajan@gmail.com>
2025-02-11 17:32:34 +0000

    Add jsdoc to MinimapCanvas component.

--- src/components/Minimap/MinimapCanvas/MinimapCanvas.tsx
+++ src/components/Minimap/MinimapCanvas/MinimapCanvas.tsx
@@ -3,6 +3,23 @@ import { log } from "../../../utils/utils";
 import styles from "./MinimapCanvas.module.css"
 import generateMinimapCanvas from "./generateMinimap";
 
+
+/**
+ * MinimapCanvas component is responsible for rendering a minimap of a given HTML element.
+ * It periodically checks for any queued redraws and forces a redraw if necessary.
+ * 
+ * @param {Object} props - The properties object.
+ * @param {HTMLElement | null} props.elementToMap - The HTML element to generate the minimap for.
+ * @param {CallableFunction} props.setMapScale - Function to track the scale of the minimap.
+ * @param {CallableFunction} props.setCanvasLoading - Function to track the loading state of the canvas.
+ * @param {boolean} props.queueRedraw - Input boolean which signals if the canvas should be redrawn.
+ * @param {CallableFunction} props.setQueueRedraw - Function used to reset the queue after canvas is drawn.
+ * 
+ * @returns {JSX.Element} The rendered MinimapCanvas component.
+ * 
+ * @component
+ * 
+ */
 const MinimapCanvas = (
   {
     elementToMap,
@@ -18,7 +35,7 @@ const MinimapCanvas = (
     queueRedraw: boolean,
     setQueueRedraw: CallableFunction
   }
-) => {
+): JSX.Element => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [checkRedraw, setCheckRedraw] = useState<boolean>(false);
   const [forceRedraw, setForceRedraw] = useState<boolean>(false);
@@ -52,6 +69,8 @@ const MinimapCanvas = (
     }
     const container = containerRef.current
     setCanvasLoading(true);
+
+    // Async function which draws the canvas inside the container.
     (async () => {
       log("Canvas drawn")
       const canvas = await generateMinimapCanvas(
